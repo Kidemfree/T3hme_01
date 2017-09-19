515fd25c3329838c09b515409ac64ec52e0447e6
Kidemfree <32015522+Kidemfree@users.noreply.github.com>
2017-09-19 21:53:03 +0100

    sdf
    sdf

--- test_mobs_field.pas
+++ test_mobs_field.pas
@@ -3,7 +3,6 @@ VAR Key:Integer;           //Right Arrow - 77
 Begin                               //Left Arrow - 75
 Repeat                            //Arrow UP - 72
 Key:=Ord(Readkey);     //Arrow Down - 80
-WriteLN(Key);               //Hello git hub
 Until key = 13;                   { 13 - Enter 01}
 End.	
 
